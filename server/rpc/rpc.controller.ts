97511b462ef74f0a117a0adbc5174006c351e8ae
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2025-12-14 07:20:56 +0000

    chore: update typesafe-rpc dependency to version 0.0.15 and remove unused express request conversion in RpcController

--- server/rpc/rpc.controller.ts
+++ server/rpc/rpc.controller.ts
@@ -22,12 +22,9 @@ export class RpcController {
 
   @Post("")
   async fetchPodVersion(@Req() request: express.Request) {
-    // Convert Express request to native Request
-    const nativeRequest = this.expressToNativeRequest(request);
-
     return createRpcHandler<RpcSchema, Context>({
       context: {
-        request: nativeRequest,
+        request,
         prisma: this.databaseService,
         appsService: this.appsService,
         updateCheckerService: this.updateCheckerService,
@@ -44,40 +41,6 @@ export class RpcController {
           }
         );
       },
-    });
-  }
-
-  private expressToNativeRequest(expressReq: express.Request): Request {
-    const protocol = expressReq.protocol || "http";
-    const host = expressReq.get("host") || "";
-    const url = `${protocol}://${host}${expressReq.originalUrl || expressReq.url}`;
-
-    // Get headers
-    const headers = new Headers();
-    Object.keys(expressReq.headers).forEach((key) => {
-      const value = expressReq.headers[key];
-      if (value) {
-        if (Array.isArray(value)) {
-          value.forEach((v) => headers.append(key, v));
-        } else {
-          headers.set(key, value);
-        }
-      }
-    });
-
-    // Get body - Express body parser should have already parsed it
-    // For native Request, we need to provide the raw body or JSON
-    const body = expressReq.body ? JSON.stringify(expressReq.body) : undefined;
-
-    // Ensure Content-Type is set if we have a body
-    if (body && !headers.has("Content-Type")) {
-      headers.set("Content-Type", "application/json");
-    }
-
-    return new Request(url, {
-      method: expressReq.method,
-      headers,
-      body,
     });
   }
 }
