51c2f4308420a1b8f9f0aba4e8692ebb76994df9
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2026-01-04 11:21:01 +0000

    feat: integrate PWA support with vite-plugin-pwa
    - Added `vite-plugin-pwa` to package.json for progressive web app capabilities.
    - Updated vite.config.ts to include PWA configuration, enabling auto-update registration and defining the app's manifest.
    - Configured manifest properties such as name, description, icons, and display settings for improved user experience.

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,10 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths({
-      projects: [path.resolve(__dirname, "tsconfig.json")],
-    }),
-    checker({ typescript: true }),
-  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -25,4 +19,33 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    react(),
+    tsconfigPaths({
+      projects: [path.resolve(__dirname, "tsconfig.json")],
+    }),
+    checker({ typescript: true }),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: { globPatterns: ["**/*.{js,css}"] },
+      manifest: {
+        id: "tn.back-n-soft.homelab-homepage",
+        name: "Homepage",
+        short_name: "Homepage",
+        description: "Homepage",
+        icons: [
+          {
+            src: "/favicon.svg",
+            sizes: "512x512",
+            purpose: "maskable",
+          },
+        ],
+        display: "standalone",
+        theme_color: "#0f172a",
+        orientation: "any",
+        start_url: "/",
+        scope: "/",
+      },
+    }),
+  ],
 });
